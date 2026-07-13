17632584a3355b261d539f017dff8450d927fcd7
Aaron <03masseyAaron@gmail.com>
2026-07-13 03:28:33 -0500

    Added config support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+const config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config.json'), 'utf-8'));
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: config.FRONTEND_PORT,
+  },
+  define: {
+    'import.meta.env.VITE_BACKEND_PORT': JSON.stringify(config.BACKEND_PORT)
+  },
   plugins: [
     vue(),
     vueDevTools(),
